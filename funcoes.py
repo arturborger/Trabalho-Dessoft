b5fca85ae1c5f604bf4a3aa922d5900311ba3ed8
Artur Borger <arturb2@al.insper.edu.br>
2026-04-28 17:08:14 -0300

    exercicio 4

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,8 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
     dado = dados_no_estoque.pop(dado_para_remover)
     dados_rolados.append(dado)
     return [dados_rolados, dados_no_estoque]
+def calcula_pontos_regra_simples(dados):
+    pontos = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    for dado in dados:
+        pontos[dado] += dado
+    return pontos
